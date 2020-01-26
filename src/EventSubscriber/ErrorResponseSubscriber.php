fbd92a3d669f6c6fcd3856b80685fbc05509d637
Muco <muco@muco.boom>
2020-01-27 01:15:21 +0300

    ErrorResponseSubscriber

---
+++ src/EventSubscriber/ErrorResponseSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ErrorResponseSubscriber implements EventSubscriberInterface
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onResponse(ResponseEvent $responseEvent){
+        $response = $responseEvent->getResponse();
+        $request = $responseEvent->getRequest();
+
+        $ip = $request->getClientIp();
+
+        $content = $response->getContent();
+
+        $content = json_decode($content, true);
+
+        if(is_array($content) && !empty($content['error'])){
+            $this->logger->info("Unauthorized API request made by ". $ip . "");
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onResponse'
+        ];
+    }
+}
