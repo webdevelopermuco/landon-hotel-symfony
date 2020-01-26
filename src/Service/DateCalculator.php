74aa0241f84fc6353e1878863cdd65d1332f2d64
Muco <muco@muco.boom>
2020-01-27 00:20:16 +0300

    fc

---
+++ src/Service/DateCalculator.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Service;
+
+
+class DateCalculator
+{
+    public function yearsDifference($year){
+        $curYear = date('Y');
+        $diff = $curYear - $year;
+        return $diff;
+    }
+}
